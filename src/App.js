6520c0e41e15762b3ed2510bf365157195ae2e7d
Lucas <laksteelhouse@gmail.com>
2021-01-29 21:56:16 +0100

    Setting up site structures in this.state for app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React from 'react'
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Lucas Knudsen",
+      headerLinks: [
+        { title: "Home", path: '/'},
+        { title: "About", path: '/about'},
+        { title: "Contact", path: '/contact'}
+      ],
+      home: {
+        title: "Code 'n' Tonic",
+        subTitle: "Fizzy creativity & bold functionality with hints of aesthetic design.",
+        text: 'Checkout these vintages:'
+      },
+      about: {
+        title: "About Me"
+      },
+      contact: {
+        title: "Let's Talk!"
+      }
+    }
+  }
+  
   render() {
 
     return (
