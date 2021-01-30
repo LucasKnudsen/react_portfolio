00850f8ac4f23e5aa13915b6448820c8c61ef00f
Lucas <laksteelhouse@gmail.com>
2021-01-30 13:08:34 +0100

    Adding projects.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import Hero from '../components/Hero'
+import Carousel from '../components/Carousel'
 
 const Home = (props) => {
   return (
-    <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+    <>
+      <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+      <Carousel />
+    </>
   )
 }
 
