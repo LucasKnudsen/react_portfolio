24dbfd4a691e22bd85758e58d7a676217fdb08f5
Lucas <laksteelhouse@gmail.com>
2021-01-30 17:48:52 +0100

    CV implemented. Starting on contact form

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import axios from 'axios'
 
-const CV = (props) => {
-  return (
-    <div>
-      <Hero title={props.title} />
-    </div>
-  )
-}
+import Hero from '../components/Hero'
+import Content from '../components/Content'
+
+class CV extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cv: {}
+    };
+  };
+  
+  componentDidMount() {
+    axios.get('./data/cv.json').then(response => {
+      this.setState({ cv: response.data })
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Container fluid={true}>
+          <Row className="justify-content-around">
+            <Col >
+            <Content>
+              <h1>{this.state.cv.title}</h1>
+              <h4>Experience:</h4>
+              <p>{this.state.cv.experience}</p>
+              <h4>Education</h4>
+              <p>{this.state.cv.education}</p>
+          </Content>
+            </Col>
+            <Col >
+            <Content>
+              <ContactForm 
+            </Content>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  };
+};
 
 export default CV
