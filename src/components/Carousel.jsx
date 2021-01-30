b345c34cbd9c35edb324f9cf55f15b3133a9afa2
Lucas <laksteelhouse@gmail.com>
2021-01-30 14:56:18 +0100

    Continueing with manual json

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,46 +10,70 @@ import podcast from '../assets/images/podcast.png'
 import raketinvestor from '../assets/images/raketinvestor.gif'
 
 class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-    };
+  state = {
+      items: [
+        {
+          id: 0,
+          title: "Raket Investor",
+          subTitle: "Build your own Investment Rocket",
+          imgSrc: raketinvestor,
+          link: "https://raketinvestor.dk",
+          selected: false
+        },
+        {
+          id: 1,
+          title: "Project 2",
+          subTitle: "Something Really Cool!",
+          imgSrc: lucas,
+          link: "#",
+          selected: false
+        },
+        {
+          id: 2,
+          title: "Podcast",
+          subTitle: "Learn more about Investing!",
+          imgSrc: "podcast.png",
+          link: "https://open.spotify.com/show/7A0vQ615Htdfl7GWN4nM6P?si=PKGwVrPFR_2EZL4tWbCziQ",
+          selected: false
+        }
+      ]
   };
 
-  componentDidMount() {
-    axios.get("./data/projects.json").then((response)=> {
-      this.setState({ items: response.data }) 
-    });
-    console.log('bla her er jeg')
-  }
-  // try to make this into hover later
-  // handleCardClick = (id, card) => {
-  //   let items = [...this.state.items];
-  //   //try with ! instead of tenary to refactor
-  //   items[id].selected = items[id].selected ? false : true
-
-  //   items.forEach(item => {
-  //     if(item.id !== id) {
-  //       item.selected = false;
-  //     }
+  // componentDidMount() {
+  //   axios.get("./data/projects.json").then((response)=> {
+  //     this.setState({ items: response.data }) 
   //   });
-
-  //   this.setState({ items })
-
-  // };
-
-  // itemList = (items) => {
-  //   return items.map(item =>{
-  //     return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id}/>
-  //   })
+  //   console.log('bla her er jeg')
   // }
+
+
+  // try to make this into hover later
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
+    //try with ! instead of tenary to refactor
+    items[id].selected = items[id].selected ? false : true
+
+    items.forEach(item => {
+      if(item.id !== id) {
+        item.selected = false;
+      }
+    });
+
+    this.setState({ items })
+
+  };
+
+  itemList = (items) => {
+    return items.map(item =>{
+      return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id}/>
+    })
+  }
 
   render () {
     return (
       <Container fluid={true}>
         <Row className="justify-content-around">
-          <h1>Hello + </h1>
+          <h1>Hello +  </h1>
         </Row>
       </Container>
     )
